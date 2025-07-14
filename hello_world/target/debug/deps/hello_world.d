e28f2ed32c5fa57efe254c9cf681c5969dd86fdb
alfianekrhmn <alfian.eka481@gmail.com>
2025-07-14 16:53:10 +0700

    feat: update hello world message and explore cargo project structure

--- hello_world/target/debug/deps/hello_world.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Alfian\Documents\Programing\PZN\rust-learning-adventure\hello_world\target\debug\deps\hello_world.exe: src\main.rs
-
-C:\Users\Alfian\Documents\Programing\PZN\rust-learning-adventure\hello_world\target\debug\deps\hello_world.d: src\main.rs
-
-src\main.rs:
--- hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Alfian\Documents\Programing\PZN\rust-learning-adventure\hello_world\target\debug\hello_world.exe: C:\Users\Alfian\Documents\Programing\PZN\rust-learning-adventure\hello_world\src\main.rs
