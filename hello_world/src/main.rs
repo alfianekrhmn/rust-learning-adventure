98f54bb2f831121e9114042429a9b0b767b0b426
alfianekrhmn <alfian.eka481@gmail.com>
2025-07-14 16:52:39 +0700

    feat: update hello world message and explore cargo project structure

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,7 +1,3 @@
 fn main() {
-    let name = "Alfian";
-    let mut age = 21;
-    println!("Halo, nama saya {} dan umur saya {} tahun", name, age);
-    age = 22;
-    println!("Sekarang umur saya adalah {} tahun", age);
+    println!("Hello")
 }
