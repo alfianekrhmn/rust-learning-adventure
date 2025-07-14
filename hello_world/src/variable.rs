41ddcb1bd4dcc78c1ce5383d773661107cfea1fd
alfianekrhmn <alfian.eka481@gmail.com>
2025-07-14 17:01:59 +0700

    feat: add example of mutable and immutable variables in Rust

---
+++ hello_world/src/variable.rs
@@ -0,0 +1,7 @@
+pub fn run() {
+    let language = "Rust";
+    let mut version = 1.0;
+    println!("Bahasa yang digunakan adalah {} dengan version {}", language, version);
+    version = 1.1;
+    println!("Sekarang bahasa {} sudah updater version, menjadi version {}", language, version);
+}
